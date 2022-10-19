0f8a965e6daaa99139e8d8724180224d338939f9
sarahmoore <sarahnmoore19@gmail.com>
2022-10-19 14:34:34 -0400

    solved problem

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,4 +1,22 @@
-// Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+
+  static getDragons(...dragons) {
+    let newArr = [];
+    dragons.forEach((dragon) => newArr.push(dragon.name));
+    return newArr;
+  }
+}
+
+
+exports.Dragon = Dragon;
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
